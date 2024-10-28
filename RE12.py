2540d000b9dedd4729867dcad3e11776e40d4b5e
SwanMichael-Dev <ckmwjrswan@yahoo.com>
2024-10-27 22:36:00 -0500

    Update and rename RE12.py to Assignment10/RE12.py

--- RE12.py
+++
@@ -1,9 +0,0 @@
-def find_words_starting_with_a_or_e(text):
-    words = text.split()
-    result = [word for word in words if word.lower().startswith(('a', 'e'))]
-    return result
-
-
-string = "The following example creates an ArrayList with a capacity of 50 elements. Four elements are then added to the ArrayList and the ArrayList is trimmed accordingly."
-words_found = find_words_starting_with_a_or_e(string)
-print(f"Words starting with 'a' or 'e': {words_found}")
